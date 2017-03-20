4562b9fea45f1669ad77766adf440247e335c467
ghq0301 <572851753@qq.com>
2017-03-20 17:43:07 +0800

    del

--- area.py
+++ area.py
@@ -1,15 +1,4 @@
-import math
-math.pi
-r=5
-print( 'r=',r,'area=',math.pi*r*r)
 
-##78.53981633974483
-##>>> import openpyxl
-##Traceback (most recent call last):
-##  File "<pyshell#4>", line 1, in <module>
-##    import openpyxl
-##ModuleNotFoundError: No module named 'openpyxl'
-##>>> 
 import math
 r1=input("请输入半径：")
 r2=int(r1)
