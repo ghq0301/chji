ab5c9ec578b073c0e1efa3b64786ec68e9a76aa4
ghq0301 <572851753@qq.com>
2017-03-20 17:40:23 +0800

    lianxi

---
+++ area.py
@@ -0,0 +1,18 @@
+import math
+math.pi
+r=5
+print( 'r=',r,'area=',math.pi*r*r)
+
+##78.53981633974483
+##>>> import openpyxl
+##Traceback (most recent call last):
+##  File "<pyshell#4>", line 1, in <module>
+##    import openpyxl
+##ModuleNotFoundError: No module named 'openpyxl'
+##>>> 
+import math
+r1=input("请输入半径：")
+r2=int(r1)
+c=2*math.pi*r2
+s=math.pi*(r2**2)
+print("c=",c,"s=",s,)
---
+++ start.py
@@ -0,0 +1,19 @@
+Python 3.6.1rc1 (v3.6.1rc1^0:e0fbe5feee4f9c00f09eb9659c2182183036261a, Mar  4 2017, 20:00:12) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print('hello world')
+hello world
+>>> 2+2
+4
+>>> 100*100
+10000
+>>> 800%3
+2
+>>> 800//3
+266
+>>> 800//%3
+SyntaxError: invalid syntax
+>>> 800//3%3
+2
+>>> 'g'+'h'+'q'
+'ghq'
+>>> 
