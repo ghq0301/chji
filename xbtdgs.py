0eadd6ca7591f5911a5722be0deb21a0d7363e25
ghq0301 <572851753@qq.com>
2017-03-21 14:00:19 +0800

    wrong

---
+++ xbtdgs.py
@@ -0,0 +1,8 @@
+a=input()
+if a=='xbt':
+    print('nh xbt')
+else:
+        print('you age?')
+        age=input()
+elif age<15:
+        print('you are not xbt')
---
+++ xbtdgs2.py
@@ -0,0 +1,5 @@
+a=input()
+if a=='xbt':
+    print('nh xbt')
+elif age<15:
+        print('you are not xbt')
